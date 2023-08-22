42c6efd0de00e119542576c8d92bc4ca7a571c54
【松田あんな】 <21241248@kyoto.ac.jp>
2023-08-22 10:19:53 +0900

    add testfile

---
+++ speciallecture/test_CSVPrinter.py
@@ -0,0 +1,27 @@
+import unittest
+from speciallecture.CSVPrinter import CSVPrinter
+
+class TestCSVPrinter(unittest.TestCase):
+
+    def test_read(self):
+        printer = CSVPrinter("sample.csv")
+        l = printer.read()
+        self.assertEqual(3,len(l))
+
+    def test_read2(self):
+        printer = CSVPrinter("sample.csv")
+        line = printer.read()
+        self.assertEqual(3,len(line))
+
+    def test_read3(self):
+        printer = CSVPrinter("sample.csv")
+        comma = printer.read()
+        self.assertEqual("old",comma[1][1])
+
+    def test_read4(self):
+        try:
+            printer = CSVPrinter("sample.csv")
+            printer.read()
+            unittest.TestCase.fail("存在なし")
+        except Exception as e:
+            pass
